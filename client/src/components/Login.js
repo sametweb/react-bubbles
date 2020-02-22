b6160d1c3a358603a439aa99d658828ddf457c02
sametweb <sametmutevellioglu@gmail.com>
2020-02-22 11:39:56 -0800

    STRETCH: react-spinners library implemented on login, overall styling edited on .scss file

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,35 +1,62 @@
 import React, { useState } from "react";
+import { css } from "@emotion/core";
 import { axiosWithAuth } from "../axiosWithAuth";
+import PacmanLoader from "react-spinners/PacmanLoader";
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
   const [creds, setCreds] = useState({ username: "", password: "" });
+  const [isLoading, setIsLoading] = useState(false);
   const { username, password } = creds;
 
   const handleChange = event =>
     setCreds({ ...creds, [event.target.name]: event.target.value });
 
-  const attemptLogin = form =>
+  const attemptLogin = form => {
+    setIsLoading(!isLoading);
     axiosWithAuth()
       .post("/login", form)
       .then(res => {
         localStorage.setItem("token", res.data.payload);
+        setIsLoading(!isLoading);
         props.history.push("/");
       })
-      .catch(() => localStorage.removeItem("token"));
+      .catch(() => {
+        setIsLoading(!isLoading);
+        localStorage.removeItem("token");
+      });
+  };
 
   const handleSubmit = event => {
     event.preventDefault();
     attemptLogin(creds);
   };
 
+  if (isLoading)
+    return (
+      <PacmanLoader
+        css={override}
+        size={100}
+        //size={"150px"} this also works
+        color={"black"}
+        loading={isLoading}
+      />
+    );
+
   return (
-    <>
+    <div className="login">
       <h1>Welcome to the Bubble App!</h1>
       <form onSubmit={handleSubmit}>
         <label htmlFor="username">
+          Username:
           <input
             id="username"
             name="username"
@@ -38,6 +65,7 @@ const Login = props => {
           />
         </label>
         <label htmlFor="password">
+          Password:
           <input
             id="password"
             name="password"
@@ -48,7 +76,7 @@ const Login = props => {
         </label>
         <button>Login</button>
       </form>
-    </>
+    </div>
   );
 };
 
